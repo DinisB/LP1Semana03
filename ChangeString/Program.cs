aee6730b254e8a3467b9b6a0281e29ca34ea6ce3
DinisB <dinisbarroso.dev@gmail.com>
2025-03-05 15:14:48 +0000

    Exercicio 2 - ChangeString

---
+++ ChangeString/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("");
+            string str = Console.ReadLine();
+            Console.Write("");
+            char.TryParse(Console.ReadLine(), out char charater);
+            str = str.Replace(charater, 'x');
+            Console.WriteLine(str);
+        }
+    }
+}
