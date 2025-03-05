c6d5548139fca2d17a2469fdd726387330f55fad
DinisB <dinisbarroso.dev@gmail.com>
2025-03-05 14:58:11 +0000

    Exercicio 1 - RightSize

---
+++ RightSize/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (var arg in args){
+                if (arg.Length > 3) {
+                    Console.WriteLine(arg);
+                }
+                else if (arg.Length >= 8) {
+                    Console.WriteLine("[EARLY STOP]");
+                    break;
+                }
+            }
+        }
+    }
+}
